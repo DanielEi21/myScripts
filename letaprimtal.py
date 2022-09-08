94a67854214310db7c81238f9c7de3484a46277e
DanielEi21 <danjov0320@skola.goteborg.se>
2022-09-08 15:37:13 +0200

    primtal

---
+++ letaprimtal.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3 
+
+
+
+#Ange max tal att prova
+numbersToTest = int(input('Hur många tal vill du undersöka?: '))
+
+#yttre loop - prova samtliga tal upp till numbersToTest
+for i in range(1,numbersToTest+1):
+	#inre loop - prova om talet i är jämt delbart med något annat tal än '1' och 'i'
+	#isf är det ej ett primtal
+	provenNotPrime = False
+	for k in range(2,i):
+		if i % k == 0:
+			print(f'{i} ej ett primtal')
+			provenNotPrime = True
+			break
+	#om vi lämnar den inre loopen utan att sätt provenNotPrime = True så har vi funnit ett primtal.
+	if provenNotPrime == False:
+		print(f'{i} är ett primtal!') 
+print('slut på program')
